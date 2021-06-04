7c07272d031247dacce74891bc4bcb82350ca4b8
mcelam00 <mcelam00@estudiantes.unileon.es>
2021-06-04 12:11:04 +0200

    Reduciendo el acoplamiento

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ const express = require('express');
 //Inicializamos express y ejecutamos el framework. App es de ahora en adelante el servidor
 const app = express();
 
+
+/*CONFIGURACIONES*/
+app.set('port', process.env.PORT || 5000); //se usará el puerto que proporcione el servicio cloud/S.O./máquina servidor y sino el 8080
+
+
+
+/*MIDDLEWARE*/
+
+
+
+
+/*ROUTES*/
+
+
+
+
 //Ponemos a escuchar el servidor
-app.listen(3000, () =>{
-    console.log('El servidor se encuentra escuchando en el puerto 3000')
+app.listen(app.get('port') , () =>{
+    console.log('El servidor se encuentra escuchando en el puerto', app.get('port'));
 });

bfcb24d9a71971816d693af50b99c2c481a75b5a
scalvd01 <scalvd01@estudiantes.unileon.es>
2021-06-11 22:27:36 +0200

    registro

--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,55 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Principal from '../views/Principal.vue'
-import MiPerfil from '../views/MiPerfil.vue'
-import Login from '../views/Login.vue'
-import ConseguirPuntos from '../views/ConseguirPuntos.vue'
-import ComprarAlbumYCartas from '../views/ComprarAlbumYCartas.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Principal from "../views/Principal.vue";
+import MiPerfil from "../views/MiPerfil.vue";
+import Login from "../views/Login.vue";
+import ConseguirPuntos from "../views/ConseguirPuntos.vue";
+import ComprarAlbumYCartas from "../views/ComprarAlbumYCartas.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [{
-        path: '/',
-        name: 'Principal',
+const routes = [
+  {
+    path: "/",
+    name: "Principal",
 
-        component: () =>
-            import ('../views/Principal.vue')
-    },
-    {
-        path: '/MiPerfil',
-        name: 'MiPerfil',
+    component: () => import("../views/Principal.vue"),
+  },
+  {
+    path: "/MiPerfil",
+    name: "MiPerfil",
 
-        component: () =>
-            import ('../views/MiPerfil.vue')
-    },
-    {
-        path: '/ConseguirPuntos',
-        name: 'ConseguirPuntos',
+    component: () => import("../views/MiPerfil.vue"),
+  },
+  {
+    path: "/ConseguirPuntos",
+    name: "ConseguirPuntos",
 
-        component: () =>
-            import ('../views/ConseguirPuntos.vue')
-    },
-    {
-        path: '/Login',
-        name: 'Login',
+    component: () => import("../views/ConseguirPuntos.vue"),
+  },
+  {
+    path: "/Login",
+    name: "Login",
 
-        component: () =>
-            import ('../views/Login.vue')
-    },
-    {
-        path: '/ComprarAlbumYCartas',
-        name: 'ComprarAlbumYCartas',
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/ComprarAlbumYCartas",
+    name: "ComprarAlbumYCartas",
 
-        component: () =>
-            import ('../views/ComprarAlbumYCartas.vue')
-    },
-]
+    component: () => import("../views/ComprarAlbumYCartas.vue"),
+  },
+  {
+    path: "/Registro",
+    name: "Registro",
+
+    component: () => import("../views/Registro.vue"),
+  },
+];
 
 const router = new VueRouter({
-    routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,15 @@
 <template>
   <v-app>
     <v-content>
-      <v-toolbar dense color="toolbarLogin" class="font-weight-medium">
-      </v-toolbar>
-
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="4">
-            <v-card width="600" class="mx-auto mt-5" justify="center" color="cardAnalisis">
+            <v-card
+              width="600"
+              class="mx-auto mt-5"
+              justify="center"
+              color="cardAnalisis"
+            >
               <v-card-title>
                 <h1 class="font-weight-medium">Login</h1>
               </v-card-title>
@@ -30,58 +32,61 @@
                 </v-form>
               </v-card-text>
               <v-divider></v-divider>
+
               <v-card-actions>
                 <template>
-  <div class="text-center">
-    <v-menu
-      v-model="menu"
-      :close-on-content-click="false"
-      :nudge-width="200"
-      offset-x
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="recuperarclave"
-          dark
-          v-bind="attrs"
-          v-on="on"
-          class="text-decoration-underline"
-        >
-          Recuperar contraseña
-        </v-btn>
-      </template>
+                  <div class="text-center">
+                    <v-menu
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :nudge-width="200"
+                      offset-x
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                          color="recuperarclave"
+                          dark
+                          v-bind="attrs"
+                          v-on="on"
+                          class="text-decoration-underline"
+                        >
+                          Recuperar contraseña
+                        </v-btn>
 
-      <v-card>
-        <v-list>
-          <v-list-item>
-            
+                        <v-btn
+                          right
+                          absolute
+                          color="success"
+                          @click="mandarDatos()"
+                          >Login</v-btn
+                        >
+                      </template>
 
-            <v-list-item-content>
-              <v-list-item-title>¿Cómo recuperar contraseña?</v-list-item-title>
-              <v-list-item-subtitle>Para recuperar la contraseña, llama al numero +34987654321</v-list-item-subtitle>
-            </v-list-item-content>
+                      <v-card>
+                        <v-list>
+                          <v-list-item>
+                            <v-list-item-content>
+                              <v-list-item-title
+                                >¿Cómo recuperar contraseña?</v-list-item-title
+                              >
+                              <v-list-item-subtitle
+                                >Para recuperar la contraseña, llama al numero
+                                +666666666
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-list>
 
-           
-          </v-list-item>
-        </v-list>
+                        <v-divider></v-divider>
+                      </v-card>
+                    </v-menu>
+                  </div> </template></v-card-actions></v-card></v-col></v-row></v-container
+    ></v-content>
 
-        <v-divider></v-divider>
-      </v-card>
-    </v-menu>
-  </div>
-</template>
-                
-                <v-spacer></v-spacer>
-                <v-btn color="success" @click="mandarDatos()"  >Login</v-btn> <!--:to="{ path: '/' }"-->
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+    <!--:to="{ path: '/' }"-->
 
     <v-footer class="toolbarLogin">
-      <v-card-text class=" .font-italic font-weight-bold text-center">
+      <v-card-text class=".font-italic font-weight-bold text-center">
         &copy;2021 — <strong>KIOSKO POKIMONS</strong>
       </v-card-text>
     </v-footer>
@@ -99,48 +104,31 @@ export default {
     };
   },
   methods: {
-    login: function() {
+    login: function () {
       console.log("loggeado");
     },
 
-    mandarDatos: function(){
+    mandarDatos: function () {
       console.log("gracias al v-model");
       console.log("Nombre =", this.name);
       console.log("Palabra de paso = ", this.password);
 
-        //peticion post que comprueba el loggueo
-        var xhttp = new XMLHttpRequest();
-        var url = 'http://localhost:5000/baseDatos/login';
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-              console.log(this.responseText);
-          }
-        };
+      //peticion post que comprueba el loggueo
+      var xhttp = new XMLHttpRequest();
+      var url = "http://localhost:5000/baseDatos/login";
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText);
+        }
+      };
 
-        xhttp.open('POST', url, true);
-            xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
-        xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
-        xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+      xhttp.open("POST", url, true);
+      xhttp.setRequestHeader("Access-Control-Allow-Headers", "*");
+      xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
+      xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
 
-
-
-
-        xhttp.send(JSON.stringify({ "name": "Olmedo", "email": "Diez" }));
-
-
-
-
-
-
-
-
-
-
-
-      
-
-    }
-
+      xhttp.send(JSON.stringify({ user: this.name, password: this.password }));
+    },
   },
   components: {},
 };

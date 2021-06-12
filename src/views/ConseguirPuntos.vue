b91a1e06ff2ea76e4db8660ab44982a12ee9cf2d
mcelam00 <mcelam00@estudiantes.unileon.es>
2021-06-12 14:08:48 +0200

    Juego Esa es la cuestion

--- src/views/ConseguirPuntos.vue
+++ src/views/ConseguirPuntos.vue
@@ -122,6 +122,69 @@
       </v-btn>
     </v-card-actions>
   </v-card>
+
+<v-card
+    class="mx-auto"
+    max-width="800"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      ¡ESA ES LA CUESTIÓN!
+    </v-card-title>
+
+    <v-card-subtitle>
+      Responde a las preguntas y gana puntos, por cada acierto se sumará un punto
+    </v-card-subtitle>
+
+    <v-divider></v-divider>
+
+       <v-btn
+      class="ma-2"
+      outlined
+      color="indigo"
+      @click="picoBoton = !picoBoton; traerPregunta()"
+      v-if="!picoBoton"
+    >
+      ¡Comencemos!
+    </v-btn>
+
+        <v-card-subtitle class="title font-weight-light" v-if="picoBoton">
+            {{pregunta}} 
+        </v-card-subtitle>
+                  <v-btn
+                          right
+                          absolute
+                  class="ma-2"
+                  outlined
+                  color="indigo"
+                   v-if="picoBoton"
+@click="traerPregunta()"
+                >
+                  Enviar
+                </v-btn>
+                
+              <v-text-field style="width:400px" class="mx-4"
+                v-model="contestacion"
+                solo
+                label="Respuesta"
+                clearable
+                v-if="picoBoton"
+              ></v-text-field>
+
+
+              
+
+
+
+  </v-card>
+
+
+
+
   <v-footer>
       <v-card-text class=".font-italic font-weight-bold text-center">
         &copy;2021 — <strong>KIOSKO POKIMONS</strong>
@@ -136,15 +199,46 @@
 export default {
   name: "ConseguirPuntos",
 
+data: () => ({
+    pregunta: "",
+    picoBoton:false,
+    contestacion: ""
+  }),
+
   data() {
     return {
       solucion1 : "si"
     };
   },
+  
   methods: {
     comprobarSolucion: function () {
 
-    }
+    },
+    traerPregunta : function(){
+    
+      console.log(this.contestacion)
+
+
+       var xhttp = new XMLHttpRequest();
+            var url = 'http://localhost:5000/Preguntas';
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              let objetoPregunta = JSON.parse(this.responseText);
+              console.log(objetoPregunta.enunciado)
+              this.pregunta = objetoPregunta.enunciado;
+            
+
+          }
+        };
+        xhttp.open('GET', url, true);
+        xhttp.send();
+
+
+
+
+
+  },
   },
 };
 

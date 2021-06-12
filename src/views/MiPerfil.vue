87b458cc28e26ba36d4e1933a8a51b3675080aea
scalvd01 <scalvd01@estudiantes.unileon.es>
2021-06-12 17:20:56 +0200

    miperfil

--- src/views/MiPerfil.vue
+++ src/views/MiPerfil.vue
@@ -1,39 +1,42 @@
 
 <template>
-  <v-container grid-list-xs>
-    <h1 >miperfil</h1>
+  <v-container fluid class="pa-8">
+    <h1>Mi Perfil</h1>
+    <h3 class="pb-2">Saldo de puntos: {{ puntos }}</h3>
+    <br />
+    <h3>Mis Álbumes:</h3>
+
+    <!---->
+    <v-row>
+      <v-col v-for="album in albumes" :key="album.nombre" cols="4">
+        <v-card max-width="344">
+          <v-img :src="album.link" class="mx-2"></v-img>
+
+          <v-card-title> {{ album.nombre }} </v-card-title>
+
+          <!--<v-card-actions>
+            <v-btn color="orange lighten-2" text router to="/"> ver mis cartas </v-btn>
+
+            <v-spacer></v-spacer>
+          </v-card-actions>-->
+        </v-card>
+      </v-col>
+    </v-row>
+    <br />
+    <h3>Mis cartas:</h3>
 
     <v-row>
-      <v-col v-for="carta in cartas" :key="carta.nombre" cols="4">
-        <v-card class="mx-auto" max-width="344">
-          <v-img :src="carta.link"></v-img>
+      <v-col v-for="carta in cartas" :key="carta.nombre" cols="2">
+        <v-card max-width="250">
+          <v-img :src="carta.link" class="mx-2"></v-img>
 
           <v-card-title> {{ carta.nombre }} </v-card-title>
 
-          <v-card-subtitle>
-            Cuesta {{ carta.precio }} puntos <br />Quedan
-            {{ carta.quedan }} unidades</v-card-subtitle
-          >
-
-          <v-card-actions>
-            <v-btn color="orange lighten-2" text> comprar </v-btn>
+          <!--<v-card-actions>
+            <v-btn color="orange lighten-2" text router to="/"> ver mis cartas </v-btn>
 
             <v-spacer></v-spacer>
-
-            <v-btn icon @click="show = !show">
-              <v-icon>{{
-                show ? "mdi-chevron-up" : "mdi-chevron-down"
-              }}</v-icon>
-            </v-btn>
-          </v-card-actions>
-
-          <v-expand-transition>
-            <div v-show="show">
-              <v-divider></v-divider>
-
-              <v-card-text> pokemon tipo mis huevos </v-card-text>
-            </div>
-          </v-expand-transition>
+          </v-card-actions>-->
         </v-card>
       </v-col>
     </v-row>
@@ -48,26 +51,85 @@ export default {
   name: "Principal",
 
   data: () => ({
-    show: false,
-    cartas: [
+    puntos: 42,
+    albumes: [
       {
         nombre: "Album Pokémon tipo Agua",
-        link:
-          "https://images-na.ssl-images-amazon.com/images/I/91sx24Dup3L._AC_SL1500_.jpg",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91sx24Dup3L._AC_SL1500_.jpg",
         precio: "3",
         quedan: "5",
       },
       {
         nombre: "Album Pokémon tipo Fuego",
-        link:
-          "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
+        link: "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
         precio: "2",
         quedan: "3",
       },
       {
         nombre: "Album Pokémon tipo Planta",
-        link:
-          "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
+        precio: "4",
+        quedan: "4",
+      },
+    ],
+    cartas: [
+      {
+        nombre: "Album Pokémon tipo Fuego",
+        link: "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
+        precio: "2",
+        quedan: "3",
+      },
+      {
+        nombre: "Album Pokémon tipo Planta",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
+        precio: "4",
+        quedan: "4",
+      },
+      {
+        nombre: "Album Pokémon tipo Fuego",
+        link: "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
+        precio: "2",
+        quedan: "3",
+      },
+      {
+        nombre: "Album Pokémon tipo Planta",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
+        precio: "4",
+        quedan: "4",
+      },
+      {
+        nombre: "Album Pokémon tipo Fuego",
+        link: "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
+        precio: "2",
+        quedan: "3",
+      },
+      {
+        nombre: "Album Pokémon tipo Planta",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
+        precio: "4",
+        quedan: "4",
+      },
+      {
+        nombre: "Album Pokémon tipo Fuego",
+        link: "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
+        precio: "2",
+        quedan: "3",
+      },
+      {
+        nombre: "Album Pokémon tipo Planta",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
+        precio: "4",
+        quedan: "4",
+      },
+      {
+        nombre: "Album Pokémon tipo Fuego",
+        link: "https://images-na.ssl-images-amazon.com/images/I/81Q7vXrh6LL._AC_SL1500_.jpg",
+        precio: "2",
+        quedan: "3",
+      },
+      {
+        nombre: "Album Pokémon tipo Planta",
+        link: "https://images-na.ssl-images-amazon.com/images/I/91MJcZ2oZ3L._AC_SL1500_.jpg",
         precio: "4",
         quedan: "4",
       },

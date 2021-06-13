b70c93d72793e8cba0a58dd131e6c5939b1ec1dd
mfernh3 <mfernh03@estudiantes.unileon.es>
2021-06-13 12:25:26 +0200

    Fotos y cosas

--- src/views/ConseguirPuntos.vue
+++ src/views/ConseguirPuntos.vue
@@ -1,9 +1,9 @@
 <template>
 <v-container grid-list-xs>
     <v-row>
-      <v-col v-for="juego in juegos" :key="juego.nombre" cols="4">
-        <v-card class="mx-auto" max-width="550">
-          <v-img :src="juego.link"></v-img>
+      <v-col v-for="juego in juegos" :key="juego.nombre" cols="6" >
+        <v-card max-width="750" height="485">
+          <v-img height="350" contain :src="juego.foto"  ></v-img>
 
           <v-card-title> {{ juego.nombre }} </v-card-title>
 
@@ -29,14 +29,16 @@ export default {
     show: false,
     juegos: [
       {
-        nombre: "Juego1",
+        nombre: "Pon a prueba tus matemáticas",
         link:
           "/Juego1",
+        foto: "https://www.educaciontrespuntocero.com/wp-content/uploads/2020/03/fondo-matematicas_23-2148146270.jpg.webp",
       },
       {
-        nombre: "Juego2",
+        nombre: "Esa es la cuestión!",
         link:
           "/Juego2",
+        foto: "https://testdivertidos.net/media/test-cultura-general/0-.jpg",
       },
       
     ],

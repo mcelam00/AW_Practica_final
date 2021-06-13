b3a0bb586d6f5c01649057ebb99636587c327f94
mfernh3 <mfernh03@estudiantes.unileon.es>
2021-06-13 17:30:33 +0200

    Juego1 hecho registro con requires

---
+++ src/components/PreguntasJuego1.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card max-width="500">
+    <v-card-title> {{ this.titulo }} </v-card-title>
+    <v-card-actions>
+      <v-text-field v-model="respondido" label="Respuesta"></v-text-field>
+
+      <v-btn
+        :color="colorBoton"
+        rounded
+        @click="comprobarSolucion()"
+      >
+        Comprobar
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+    props: {
+        titulo : String,
+        respuesta : String
+    },
+  data() {
+    return {
+        colorBoton:"white",
+        respondido: "",
+    };
+  },
+
+  methods: {
+    comprobarSolucion: function() {
+        console.log(this.titulo + " " + this.respuesta+ " " +this.respondido)
+      if (this.respuesta == this.respondido) {
+        this.colorBoton = "green";
+      } else {
+        this.colorBoton = "red";
+      }
+    },
+  },
+};
+</script>
--- src/views/Juego1.vue
+++ src/views/Juego1.vue
@@ -1,181 +1,47 @@
 <template>
-<v-container>
-  <v-card
-    class="mx-auto mt-8"
-    max-width="700"
-    outlined
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
-          Pregunta 1
-        </div>
-        <v-list-item-title class="text-h5 mb-1">
-          1+1
-        </v-list-item-title>
-      </v-list-item-content>
+  <v-container>
+    <v-col v-for="(pregunta, i) in this.preguntas" :key="i">
+      <PreguntasJuego1
+        :titulo = "pregunta[0]" 
+        :respuesta = "pregunta[1]">
+      </PreguntasJuego1>
+    </v-col>
 
-     
-    </v-list-item>
-    
-
-    <v-card-actions>
-<v-text-field
-            v-model="title"
-            :rules="respuesta1"
-            label="Respuesta"
-          ></v-text-field>   
-<v-btn
-        outlined
-        rounded
-        text
-      >
-        Comprobar
-      </v-btn>
-    </v-card-actions>
-    
-    <!--inicio de la siguiente pregunta-->
-
-
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
-          Pregunta 2
-        </div>
-        <v-list-item-title class="text-h5 mb-1">
-          ¿?
-        </v-list-item-title>
-      </v-list-item-content>
-
-     
-    </v-list-item>
-    <v-card-actions>
-<v-text-field
-            v-model="title"
-            :rules="respuesta"
-            label="Respuesta"
-          ></v-text-field>   
-<v-btn
-        outlined
-        rounded
-        text
-      >
-        Comprobar
-      </v-btn>
-    </v-card-actions>
-    <!--inicio de la siguiente pregunta-->
-
-
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
-          Pregunta 3
-        </div>
-        <v-list-item-title class="text-h5 mb-1">
-          ¿?
-        </v-list-item-title>
-      </v-list-item-content>
-
-     
-    </v-list-item>
-    <v-card-actions>
-<v-text-field
-            v-model="title"
-            :rules="respuesta"
-            label="Respuesta"
-          ></v-text-field>   
-<v-btn
-        outlined
-        rounded
-        text
-      >
-        Comprobar
-      </v-btn>
-    </v-card-actions>
-    <!--inicio de la siguiente pregunta-->
-
-
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
-          Pregunta 4
-        </div>
-        <v-list-item-title class="text-h5 mb-1">
-          ¿?
-        </v-list-item-title>
-      </v-list-item-content>
-
-     
-    </v-list-item>
-    <v-card-actions>
-<v-text-field
-            v-model="title"
-            :rules="respuesta"
-            label="Respuesta"
-          ></v-text-field>   
-<v-btn
-        outlined
-        rounded
-        text
-      >
-        Comprobar
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-
-
-
-
-
-
-  <v-footer>
+    <v-footer>
       <v-card-text class=".font-italic font-weight-bold text-center">
         &copy;2021 — <strong>KIOSKO POKIMONS</strong>
       </v-card-text>
     </v-footer>
-    </v-container>
+  </v-container>
 </template>
 
 <script>
+import PreguntasJuego1 from "@/components/PreguntasJuego1";
 export default {
-  name: "ConseguirPuntos",
+  name: "ConseguirPuntosJuego1",
+
+  components: {
+    PreguntasJuego1,
+  },
 
   data() {
     return {
-      solucion1 : "si",
-    picoBoton:false,
-    contestacion: ""
+      colorBoton: [],
+      title: [],
+      preguntas: [["1 + 1", "2"], ["32 + 54", "86"], ["12 · 13", "156"], ["34/2", "17"], ["√144", "12"]],
+      respuestas: ["2", "86", "156", "17", "12"],
     };
   },
-  
+
   methods: {
-    comprobarSolucion: function () {
-
+    comprobarSolucion: function(valor, i, colorBoton) {
+      if (valor == this.respuestas[i]) {
+        this.colorBoton[i] = "green";
+        console.log("me cago en dios");
+      } else {
+        this.colorBoton[i] = "red";
+      }
     },
-    traerPregunta : function(){
-    
-      console.log(this.contestacion)
-
-
-       var xhttp = new XMLHttpRequest();
-            var url = 'http://localhost:5000/Preguntas';
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-
-              let objetoPregunta = JSON.parse(this.responseText);
-              document.getElementById("pregunta").innerHTML = objetoPregunta.enunciado;
-
-          }
-        };
-        xhttp.open('GET', url, true);
-        xhttp.send();
-
-
-
-
-
-  },
   },
 };
-
 </script>
